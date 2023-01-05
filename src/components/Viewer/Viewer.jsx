c32e7b109d088534530d45cc463627d6d65e0090
Cattamoo <ouob77@kakao,com>
2023-01-05 18:41:06 +0900

    Viewer에 ReactMarkdown 추가

--- src/components/Viewer/Viewer.jsx
+++ src/components/Viewer/Viewer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './Viewer.module.css';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 export default function Viewer({ text }) {
 	return (
-		<div className={styles.viewer} dangerouslySetInnerHTML={{ __html: text }} />
+		<div className={styles.viewer}>
+			<ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+				{ text && text.replaceAll('\\n', '\n') }
+			</ReactMarkdown>
+		</div>
 	);
 }
