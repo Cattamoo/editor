9fb27f7eddd1c851d7e33346c507f2a3cf928971
Cattamoo <ouob77@kakao,com>
2023-01-06 14:01:30 +0900

    파일이름 설정 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from './Header.module.css';
 
 export default function Header({ text, setText }) {
 	const fileRef = useRef();
+	const [fileName, setFileName] = useState('New Markdown');
 	const handleUpload = ({ target }) => {
 		const file = target.files[0];
 		if(file) {
 			const fileReader = new FileReader();
 			fileReader.onload = () => {
+				setFileName(file.name.replace(/(\.\w+)/, ''));
 				setText(fileReader.result);
 			}
 			fileReader.readAsText(file);
 		}
 	}
 	const handleUploadClick = () => {
-		fileRef.current.click();
+		fileRef.current?.click();
 	}
 	const handleSave = () => {
 		const a = document.createElement('a');
 		const file = new Blob([text], {
 			type: 'text/plain'
-		})
-		const fileName = `NewMarkdown_${new Date().getTime()}`;
+		});
 		a.href = URL.createObjectURL(file);
 		a.download = `${fileName}.md`;
 		a.click();
@@ -30,7 +31,8 @@ export default function Header({ text, setText }) {
 		<header className={styles.header}>
 			<span className={styles.logo}>Markdown Editor📝</span>
 			<div>
-				<input className="hidden" ref={fileRef} type="file" onChange={handleUpload} accept=".md" />
+				<input type="text" value={`${fileName}`} onChange={({ target }) => setFileName(target.value)} />
+				<input ref={fileRef} type="hidden" onChange={handleUpload} accept=".md" />
 				<button className={styles.btn} onClick={handleUploadClick}>Upload</button>
 				<button className={styles.btn} onClick={handleSave}>Save</button>
 			</div>
